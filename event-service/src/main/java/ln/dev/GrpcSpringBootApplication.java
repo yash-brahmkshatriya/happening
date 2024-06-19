20516895b6ad93e7162ab76b41fb61dd7ed71fb6
Yash Brahmkshatriya <yash311000@gmail.com>
2024-06-19 23:30:36 +0530

    gRPC spring boot application

---
+++ event-service/src/main/java/ln/dev/GrpcSpringBootApplication.java
@@ -0,0 +1,11 @@
+package ln.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcSpringBootApplication.class, args);
+    }
+}
