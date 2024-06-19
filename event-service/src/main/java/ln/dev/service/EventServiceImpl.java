b5c0291128ff09ada8af76004d5ffc0f2aad2158
Yash Brahmkshatriya <yash311000@gmail.com>
2024-06-19 23:29:58 +0530

    Heartbeat RPC implementation

---
+++ event-service/src/main/java/ln/dev/service/EventServiceImpl.java
@@ -0,0 +1,33 @@
+package ln.dev.service;
+
+import io.grpc.stub.StreamObserver;
+import ln.dev.grpc.EventRequest;
+import ln.dev.grpc.EventServiceGrpc;
+import ln.dev.grpc.HeartbeatResponse;
+import ln.dev.protos.common.Common;
+import ln.dev.protos.event.Event;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@GrpcService
+public class EventServiceImpl extends EventServiceGrpc.EventServiceImplBase {
+    @Override
+    public void streamEvents(EventRequest request, StreamObserver<Event> responseObserver) {
+
+    }
+
+    @Override
+    public void heartbeat(Common.Empty request, StreamObserver<HeartbeatResponse> responseObserver) {
+        HeartbeatResponse heartbeatResponse = HeartbeatResponse.newBuilder()
+                .setMessage("Heartbeat success")
+                .setTimestamp(
+                        new SimpleDateFormat("yyyy-MM-dd HH.mm.ss").format(new Date())
+                )
+                .build();
+
+        responseObserver.onNext(heartbeatResponse);
+        responseObserver.onCompleted();
+    }
+}
